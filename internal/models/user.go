1044c36cab9481e8d41d541544292b8713376b81
Joram Wambugu <artisan254@gmail.com>
2022-09-02 21:52:24 +0300

    Refactor: user structs from generate protobuf

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,30 +1,14 @@
 package models
 
 import (
+	"bridge/api/v1/pb"
 	"database/sql/driver"
 	"encoding/json"
 	"errors"
-	"time"
 )
 
-type User struct {
-	ID          uint64     `json:"id,omitempty" db:"id"`
-	Name        string     `json:"name,omitempty" db:"name"`
-	Email       string     `json:"email,omitempty" db:"email"`
-	PhoneNumber string     `json:"phone_number,omitempty"`
-	Password    string     `json:"password,omitempty" db:"password"`
-	Meta        UserMeta   `json:"meta,omitempty" db:"meta"`
-	CreatedAt   *time.Time `json:"created_at,omitempty" db:"created_at"`
-	UpdatedAt   *time.Time `json:"updated_at,omitempty" db:"updated_at"`
-}
-
-type KYCData struct {
-	IDNumber string `json:"id_number,omitempty"`
-	KRAPin   string `json:"kra_pin,omitempty"`
-}
-
 type UserMeta struct {
-	KYCData KYCData `json:"kyc_data,omitempty"`
+	pb.UserMeta
 }
 
 // Value implements driver.Valuer which simply returns the JSON-encoded representation of UserMeta.
